4ed2eae8557a275c640daac1151149695aece149
Pro100sasha_HD <sasha_ablin@rambler.ru>
2024-11-28 23:08:51 +0500

    Task1

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,39 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public class Pair<A, B> {
+    private final A Value1;
+    private final B Value2;
+    private Pair(A value1, B value2)
+    {
+        this.Value1 = value1;
+        this.Value2 = value2;
+    }
+    public A getFirst() {
+        return Value1;
+    }
+    public B getSecond() {
+        return Value2;
+    }
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || o.getClass() != getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(Value1, pair.Value1) && Objects.equals(Value2, pair.Value2);
+    }
+    public int hashCode() {
+        return Objects.hash(Value1, Value2);
+    }
+    public static <A, B> Pair<A, B> of(A value1, B value2) {
+        return new Pair<>(value1, value2);
+    }
+    public void ifPresent(BiConsumer<? super A, ? super B> consumer) {
+        if (Value1 != null && Value2 != null) {
+            consumer.accept(Value1, Value2);
+        }
+    }
 }
